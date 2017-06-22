85d7292f40838756f35865e287d15e7c932a5a35
Jiemy <15271821083@163.com>
2017-06-22 19:12:46 +0800

    修复删除考生bug2 6.22

--- manager/controllers/kaosheng.php
+++ manager/controllers/kaosheng.php
@@ -532,16 +532,14 @@ class Kaosheng extends CI_Controller {
 		$this->load->model ( 'Data_model' );
 		
 		$que=$this->Data_model->get_alldata ( $id, 'student_exam','exam' );
-// 		$re=array();
-// 		$rea=array();
 		foreach($que as $resp){
 			$rea=$resp['id'];
 			$re=$resp['number']-1;
+			//更新考试的已选人数
 			$this->Data_model->update_data( $rea,array('number'=>$re), 'exam' );
 		}
 		
-		//更新考试的已选人数
-// 		$data['query3']=$this->Data_model->update_data2( $id,'student_exam', 'exam' );
+		
 		//删除学生_考试表中的已选人数
 		$data ['query2'] = $this->Data_model->delete_data( $id, 'student_exam' );
 		//删除学生表中的信息
