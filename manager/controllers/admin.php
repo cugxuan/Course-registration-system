20e20d76257d76b6afbb23f1c8a6e0045738e07d
cugxuan <179562600@qq.com>
2017-06-27 12:05:53 +0000

    version 1.0

--- manager/controllers/admin.php
+++ manager/controllers/admin.php
@@ -300,9 +300,9 @@ class Admin extends CI_Controller {
 	
 	    //查询数据库提示信息放置到str
 	    $this->load->model ( 'Data_model' );
-	    $info= $this->Data_model->get_adata ( 1, 'info' );  //1表示考生信息
-	    $list['str1']=$info['info'];
 	    $info= $this->Data_model->get_adata ( 0, 'info' );  //0表示注册信息
+	    $list['str1']=$info['info'];
+	    $info= $this->Data_model->get_adata ( 1, 'info' );  //1表示考生信息
 	    $list['str2']=$info['info'];
 	    
 		$data ['title'] = '修改公告栏  - ';
@@ -321,34 +321,14 @@ class Admin extends CI_Controller {
 			echo 'window.location.href="' . site_url ( 'index' ) . '";</script>';
 			exit ();
 		}
-	
-		$id=0;
-		$info=htmlspecialchars($this->input->post('str1'));
-		$info2=htmlspecialchars($this->input->post('str2'));
-		//$info=$this->input->post('str1');
-// 		$id=substr($str, 0, 1);
-// 		$info=substr($str, 1);
- 		$status ['msg'] = '修改成功！'.$info.'end';
- 		$this->load->view ( 'status', $status );
-		
-		
-// 		//对数据库中的内容进行更新
-// 		$this->load->model ( 'Data_model' );
-// 		$query=$this->Data_model->update($id,array('info'=>$info),'info' );
-
-
-// 		//查询数据库提示信息放置到str
-// 		$info= $this->Data_model->get_adata ( 1, 'info' );  //1表示考生信息
-// 		$list['str1']=$info['info'];
-// 		$info= $this->Data_model->get_adata ( 0, 'info' );  //0表示注册信息
-// 		$list['str2']=$info['info'];
-// 		$data ['title'] = '修改公告栏  - ';
-// 		$data ['curbig'] = 6; // current
-// 		$data ['cursmal'] = 61; // class="current"
-		
-		
-		$this->load->view ( 'menu', $data );
-		$this->load->view ( 'public_edit', $list );
+		$this->load->model ( 'Data_model' );
+	     $str1 = $_POST['str1'] ? $_POST['str1'] : '';
+	     $str2 = $_POST['str2'] ? $_POST['str2'] : '';
+	     $test =  $_POST['test'] ? $_POST['test'] : '';
+	     $res1 = $this->Data_model->update_data ( 0, array('info'=>$str1), 'info' );
+	     $res2 = $this->Data_model->update_data ( 1, array('info'=>$str2), 'info' );
+	     
+	     redirect('admin/edit_info');
 	}
 }
 ?>
--- manager/views/public_edit.php
+++ manager/views/public_edit.php
@@ -7,21 +7,13 @@
 <h2><center><?php echo 修改公告栏信息;?></center></h2>
 <form action="<?php echo site_url('admin/edit_info_do/'); ?>" method="post" id="form" >
             <fieldset>
-            <div>
+            
             	<span class="gg button" style="background: red url(../images/bg-button-green.gif) top left repeat-x !important;">公告1</span>
-            	<div class="str" style="display: none">			
-					<textarea  id="str1" name="str1"  cols="20" rows="10"><?php if(isset($str1))  {echo $str1;}?>
-					</textarea>
-				</div>
-			</div>
-			<div>
+					<textarea class="str" id="str1" name="str1"  cols="20" rows="10" style="display: none"><?php if(isset($str1))  {echo $str1;}?></textarea>
+				<br />			
 				<span class="gg button" style="background: red url(../images/bg-button-green.gif) top left repeat-x !important;">公告2</span>
-				<div class="str" style="display: none">
-					<textarea   id="str2" name="str2"  cols="20" rows="10"><?php if(isset($str2))  {echo $str2;}?>
-					</textarea>
-				</div>
-			</div>	
-			
+					<textarea class="str" id="str2" name="str2"  cols="20" rows="10" style="display: none"><?php if(isset($str2))  {echo $str2;}?></textarea>				
+                <br />
              <input class="button" type="submit" value="提交修改" />
             </fieldset>
             <div class="clear"></div>
